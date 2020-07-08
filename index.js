df94a354e38d5192275bdece8c509b57e39ea4fd
OKOPOK <self@BOPOHOB.ru>
2020-07-08 21:31:09 +0300

    move webascm to npm package manager

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const js = import("./node_modules/timecalc/timecalc.js");
+const js = import("./node_modules/@bopohob/timecalc/timecalc.js");
+const input = document.getElementById("input");
+const storageKey = "expr";
 js.then(js => {
-  const input = document.getElementById("input");
   const update = () => {
     let result = "";
+    const value = document.getElementById("input").value;
+    localStorage.setItem(storageKey, value);
     try {
-      result = js.eval_string(document.getElementById("input").value);
+      result = js.eval_string(value);
     } catch(exception) {
       console.log(exception.error);
       result = exception.description;
@@ -15,3 +18,4 @@ js.then(js => {
   input.addEventListener("keydown", update);
   input.addEventListener("keyup", update);
 });
+input.value = localStorage.getItem(storageKey) || "45:00 / 1:52 * 3.921";
