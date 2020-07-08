b28dfca3c760be24a7dfd48eda305917e13d2718
OKOPOK <self@BOPOHOB.ru>
2020-07-09 02:59:40 +0300

    move errors text rendering to js

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 const errorDebriefing = error => {
-
+  if (error.Function) {
+    const f = error.Function;
+    if (f[1] === "UnknownFunction") {
+      return `${f[0]} function is unknown`;
+    } else if ("NumberArgs" in f[1]) {
+      return `${f[0]} require ${f[1].NumberArgs} argument${f[1].NumberArgs === 1 ? '' : 's'}`;
+    }
+  }
+  if (error.ParseError) {
+    const f = error.ParseError;
+    if ("MissingArgument" === f) {
+      return "Missing argument at the end of expression";
+    } else if ("UnexpectedToken" in f) {
+      return `Unexpected token in position ${f.UnexpectedToken}`;
+    } else if ("MissingRParen" in f) {
+      return f.MissingRParen == 1 ? "Missing right parenthesis" : `Missing ${f.MissingRParen} right parentheses`;
+    }
+  }
+  if (error.RPNError) {
+    const f = error.RPNError;
+    if (f.MismatchedLParen) {
+      return `Mismatched left parenthesis at token ${f.MismatchedLParen}`;
+    }
+    if (f.MismatchedRParen) {
+      return `Mismatched right parenthesis at token ${f.MismatchedRParen}`;
+    }
+    if (f.UnexpectedComma) {
+      return `Unexpected comma at token ${f.UnexpectedComma}`;
+    }
+    if (f.NotEnoughOperands) {
+      return `Missing operands at token ${f.NotEnoughOperands}`;
+    }
+    if (f.TooManyOperands) {
+      return "Too many operands left at the end of expression";
+    }
+  }
+  if (error.EvalError) {
+    const f = error.EvalError;
+    if (f.FactorialError) {
+      return "Numeric fail while factorial computation";
+    }
+  }
 }
 
 const js = import("./node_modules/@bopohob/timecalc/timecalc.js");
